7fe332742c8359263e98ff67f9db49710d77bf5a
SweetChristabel <simonamit@protonmail.com>
2026-03-20 01:29:35 +0100

    finished webapp homework

--- app.py
+++ app.py
@@ -6,13 +6,17 @@ app = Flask(__name__) #creates a Flask application
 def home():
     return render_template("index.html")
 
-@app.route("/rollerskate")
-def rollerskate():
-    return render_template("rollerskate.html")
+@app.route("/muchmore")
+def therewillbemore():
+    return render_template("more.html")
 
 @app.route("/nerd")
 def nerd():
     return render_template("nerd.html")
+
+@app.route("/aboutflask")
+def school():
+    return render_template("school.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
